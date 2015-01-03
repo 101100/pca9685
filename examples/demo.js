0ee18fac6385446b14c5a341d58345068ae9bbdc
Jason Heard <jasonpheard@gmail.com>
2015-01-03 19:14:23 +0000

    Clean up code to use callback once initialized

--- examples/demo.js
+++ examples/demo.js
@@ -4,36 +4,27 @@ var Pca9685Driver = require("../pca9685"),
     I2C = require("i2c"),
     options = {
         i2c: new I2C(0x40, {device: "/dev/i2c-1"}),
-        frequency: 50,
+        frequency: 100,
         debug: true
     },
-    pwm = new Pca9685Driver(options),
-    pulseLengths,
+    pwm,
+    // pulse lengths in microseconds (theoretically, 1.5 ms is the middle of the servo's range)
+    pulseLengths = [ 1200, 1500, 1700 ],
     nextPulse = 0,
     timer,
-    cycleLengthMicroSeconds = 1000000 / options.frequency,
-    stepLengthMicroSeconds = cycleLengthMicroSeconds / 4096;
+    steeringChannel = 0;
 
 
-function setServoPulse(channel, pulseLengthMicroSeconds) {
-    var onStep = 0;
-    var offStep = Math.round(pulseLengthMicroSeconds / stepLengthMicroSeconds) - 1;
-
-    pwm.setPulseRange(channel, onStep, offStep);
-}
-
-
+// loop to cycle through pulse lengths
 function servoLoop() {
     timer = setTimeout(servoLoop, 500);
 
-    setServoPulse(0, pulseLengths[nextPulse]);
+    pwm.setPulseLength(steeringChannel, pulseLengths[nextPulse]);
     nextPulse = (nextPulse + 1) % pulseLengths.length;
 }
 
 
-// pulse lengths in microseconds (theoretically, 1.5 ms is the middle)
-pulseLengths = [ 1400, 1600, 1800 ];
-
+// set-up CTRL-C with graceful shutdown
 process.on('SIGINT', function () {
     console.log("\nGracefully shutting down from SIGINT (Ctrl-C)");
 
@@ -45,4 +36,14 @@ process.on('SIGINT', function () {
     pwm.allChannelsOff();
 });
 
-servoLoop();
+
+// initialize PCA9685 and start loop once initialized
+pwm = new Pca9685Driver(options, function (err) {
+    if (err) {
+        console.error("Error initializing PCA9685");
+        process.exit(-1);
+    }
+
+    console.log("Starting servo loop...");
+    servoLoop();
+});
--- pca9685.js
+++ pca9685.js
@@ -8,25 +8,29 @@
 
 var constants = {
     modeRegister1: 0x00, // MODE1
-    LED0_ON_L: 0x06, // LED0 on time low byte
-    LED0_ON_H: 0x07, // LED0 on time high byte
-    LED0_OFF_L: 0x08, // LED0 off time low byte
-    LED0_OFF_H: 0x09, // LED0 off time low byte
-    ALLLED_ON_L: 0xFA,
-    ALLLED_ON_H: 0xFB,
-    ALLLED_OFF_L: 0xFC,
-    ALLLED_OFF_H: 0xFD,
-    PRESCALE: 0xFE
+    modeRegister2: 0x01, // MODE2
+    channel0OnStepLowByte: 0x06, // LED0_ON_L
+    channel0OnStepHighByte: 0x07, // LED0_ON_H
+    channel0OffStepLowByte: 0x08, // LED0_OFF_L
+    channel0OffStepHighByte: 0x09, // LED0_OFF_H
+    registersPerChannel: 4,
+    allChannelsOnStepLowByte: 0xFA, // ALL_LED_ON_L
+    allChannelsOnStepHighByte: 0xFB, // ALL_LED_ON_H
+    allChannelsOffStepLowByte: 0xFC, // ALL_LED_OFF_L
+    allChannelsOffStepHighByte: 0xFD, // ALL_LED_OFF_H
+    preScale: 0xFE, // PRE_SCALE
+    stepsPerCycle: 4096
 };
 
 
-function Pca9685Driver(options) {
+function Pca9685Driver(options, cb) {
     this.i2c = options.i2c;
     this.debug = !!options.debug;
     this.frequency = options.frequency;
+    var cycleLengthMicroSeconds = 1000000 / options.frequency;
+    this.stepLengthMicroSeconds = cycleLengthMicroSeconds / constants.stepsPerCycle;
 
     this._send = this._send.bind(this);
-    this._step2 = this._step2.bind(this);
 
     if (this.debug) {
         console.log("Reseting PCA9685");
@@ -34,8 +38,60 @@ function Pca9685Driver(options) {
 
     this._send(constants.modeRegister1, 0x00);
 
-    this._setFrequency(this.frequency);
+    this.allChannelsOff();
+    this._setFrequency(this.frequency, cb);
 }
+
+
+function createSetFrequencyStep2(sendFunc, debug, prescale, cb) {
+    cb = typeof cb === "function" ? cb : function () { return; };
+
+    return function setFrequencyStep2(err, res) {
+        if (err) {
+            if (debug) {
+                console.log("error", err);
+            }
+            cb(err);
+        }
+
+        var oldmode = res[0],
+            newmode = (oldmode & 0x7F) | 0x10;
+
+        if (debug) {
+            console.log("Setting prescale to:", prescale);
+        }
+
+        sendFunc(constants.modeRegister1, newmode);
+        sendFunc(constants.preScale, Math.floor(prescale));
+        sendFunc(constants.modeRegister1, oldmode);
+
+        // documentation says that 500 microseconds are required
+        // before restart is sent, so a timeout of 10 milliseconds
+        // should be plenty
+        setTimeout(function () {
+            if (debug) {
+                console.log("Restarting controller");
+            }
+
+            sendFunc(constants.modeRegister1, oldmode | 0x80);
+
+            cb();
+        }, 10);
+    };
+}
+
+
+Pca9685Driver.prototype._setFrequency = function setPwmFrequency(freq, cb) {
+    // 25MHz base clock, 12 bit (4096 steps per cycle)
+    var prescale = Math.round(25000000 / (constants.stepsPerCycle * freq)) - 1;
+
+    if (this.debug) {
+        console.log("Setting PWM frequency to", freq, "Hz");
+        console.log("Pre-scale value:", prescale);
+    }
+
+    this.i2c.readBytes(constants.modeRegister1, 1, createSetFrequencyStep2(this._send, this.debug, prescale, cb));
+};
 
 
 Pca9685Driver.prototype._send = function sendCommand(cmd, values) {
@@ -51,64 +107,47 @@ Pca9685Driver.prototype._send = function sendCommand(cmd, values) {
 };
 
 
-Pca9685Driver.prototype._setFrequency = function setPwmFrequency(freq) {
-    // 25MHz, 12 bit (/ 4096)
-    var prescale = Math.round(25000000 / (4096 * freq)) - 1;
-
-    if (this.debug) {
-        console.log("Setting PWM frequency to", freq, "Hz");
-        console.log("Pre-scale value:", prescale);
-    }
-
-    this.prescale = prescale;
-    this.i2c.readBytes(constants.modeRegister1, 1, this._step2);
-};
-
-
-Pca9685Driver.prototype._step2 = function readStep2(err, res) {
-    if (err) {
-        console.log("error", err);
-        throw new Error(err);
-    }
-
-    var oldmode = res[0],
-        newmode = (oldmode & 0x7F) | 0x10,
-        prescale = this.prescale;
-
-    if (this.debug) {
-        console.log("Setting prescale to:", prescale);
-    }
-
-    this._send(constants.modeRegister1, newmode);
-    this._send(constants.PRESCALE, Math.floor(prescale));
-    this._send(constants.modeRegister1, oldmode);
-
-    // documentation says that 500 microseconds are required
-    // before restart is sent
-    setTimeout(function () {
-        if (this.debug) {
-            console.log("Restarting controller");
-        }
-
-        this._send(constants.modeRegister1, oldmode | 0x80);
-    }.bind(this), 10);
-};
-
-
 Pca9685Driver.prototype.setPulseRange = function setPwmRange(channel, onStep, offStep) {
     if (this.debug) {
         console.log("Setting PWM channel, channel:", channel, "onStep:", onStep, "offStep:", offStep);
     }
 
-    this._send(constants.LED0_ON_L + 4 * channel, onStep & 0xFF);
-    this._send(constants.LED0_ON_H + 4 * channel, onStep >> 8);
-    this._send(constants.LED0_OFF_L + 4 * channel, offStep & 0xFF);
-    this._send(constants.LED0_OFF_H + 4 * channel, offStep >> 8);
+    this._send(constants.channel0OnStepLowByte + constants.registersPerChannel * channel, onStep & 0xFF);
+    this._send(constants.channel0OnStepHighByte + constants.registersPerChannel * channel, (onStep >> 8) & 0x0F);
+    this._send(constants.channel0OffStepLowByte + constants.registersPerChannel * channel, offStep & 0xFF);
+    this._send(constants.channel0OffStepHighByte + constants.registersPerChannel * channel, (offStep >> 8) & 0x0F);
+};
+
+
+Pca9685Driver.prototype.setPulseLength = function setPwmPulseLength(channel, pulseLengthMicroSeconds, onStep) {
+    onStep = onStep || 0;
+
+    if (this.debug) {
+        console.log("Setting PWM channel, channel:", channel, "pulseLength:", pulseLengthMicroSeconds, "onStep:", onStep);
+    }
+
+    var offStep = (onStep + Math.round(pulseLengthMicroSeconds / this.stepLengthMicroSeconds) - 1) % constants.stepsPerCycle;
+
+    this.setPulseRange(channel, onStep, offStep);
+};
+
+
+Pca9685Driver.prototype.setDutyCycle = function setPwmPulseLength(channel, dutyCycleDecimalPercentage, onStep) {
+    onStep = onStep || 0;
+
+    if (this.debug) {
+        console.log("Setting PWM channel, channel:", channel, "dutyCycle:", dutyCycleDecimalPercentage, "onStep:", onStep);
+    }
+
+    var offStep = (onStep + Math.round(dutyCycleDecimalPercentage * constants.stepsPerCycle) - 1) % constants.stepsPerCycle;
+
+    this.setPulseRange(channel, onStep, offStep);
 };
 
 
 Pca9685Driver.prototype.allChannelsOff = function stopAllMotors() {
-    this._send(constants.ALLLED_OFF_H, 0x01);
+    // Setting the high byte to 1 will turn off the channel
+    this._send(constants.allChannelsOffStepHighByte, 0x01);
 };
 
 
